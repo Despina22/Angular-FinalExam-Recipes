1dbdc42da55cfe8db52e7abd9f3bd0b25360f4d6
Despina22 <stankovskadespina22@gmail.com>
2023-09-07 01:59:52 +0200

    vercel deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseApiUrl:
-    'https://my-json-server.typicode.com/Despina22/Angular-FinalExam-Recipes/',
+    'https://recipes-json-server.vercel.app/api/',
 };
